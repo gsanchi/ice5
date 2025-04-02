427ab01d40c1dddfe5d64b9755b7504c634017de
gsanchi <177684071+gsanchi@users.noreply.github.com>
2025-04-02 10:40:14 -0600

    second push

--- src/elevator_controller_fsm.vhd
+++ src/elevator_controller_fsm.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : MooreElevatorController.vhd
---| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson, Capt Brian Yarbrough, ***YourName***
---| CREATED       : 03/2018 Last Modified on 06/24/2020
+--| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson, Capt Brian Yarbrough, Gia Sanchirico
+--| CREATED       : 03/2018 Last Modified on 04/2/2025
 --| DESCRIPTION   : This file implements the ICE5 Basic elevator controller (Moore Machine)
 --|
 --|  The system is specified as follows:
@@ -87,7 +87,7 @@ architecture Behavioral of elevator_controller_fsm is
     -- "sm_floor" the same way you'd assign a signal as std_logic
 	type sm_floor is (s_floor1, s_floor2, s_floor3, s_floor4);
 	
-	-- Here you create variables that can take on the values defined above. Neat!	
+	-- Here you create variables that can take on the values defined agit cobove. Neat!	
 	signal f_Q, f_Q_next: sm_floor;
 
 begin
@@ -95,15 +95,45 @@ begin
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	
 	-- Next State Logic
+    f_Q_next <= s_floor2 when (i_up_down = '1' and f_Q = s_floor1) else -- going up
+                s_floor3 when (i_up_down = '1' and f_Q = s_floor2) else
+                s_floor4 when (i_up_down = '1' and f_Q = s_floor3) else
+                
+                --Going Down
+                s_floor3 when (i_up_down = '0' and f_Q = s_floor4) else 
+                s_floor2 when (i_up_down = '0' and f_Q = s_floor3) else
+                s_floor1 when (i_up_down = '0' and f_Q = s_floor2) else
+                
+                s_floor1; -- default case
   
 	-- Output logic
+    with f_Q select
+        o_floor <= "0001" when s_floor1,
+                   "0010" when s_floor2,
+                   "0011" when s_floor3,
+                   "0100" when s_floor4,
+                   "0001" when others;  --default is floor 1
+    
+    
 
 	-------------------------------------------------------------------------------------------------------
 	
 	-- PROCESSES ------------------------------------------------------------------------------------------	
 	
 	-- State register ------------
-	
+	--Synchronous reset
+	register_proc : process (i_clk)
+    begin
+        if rising_edge(i_clk) then
+            if i_reset = '1' then
+                f_Q <= s_floor2;        -- reset state is OFF
+            elsif (i_stop = '0') then
+                f_Q <= f_Q_next;    -- next state becomes current state
+            else
+                f_Q <= f_Q;
+            end if;
+        end if;
+    end process register_proc;
 	
 	-------------------------------------------------------------------------------------------------------
 	
--- src/elevator_controller_fsm_tb.vhd
+++ src/elevator_controller_fsm_tb.vhd
@@ -110,19 +110,34 @@ begin
 		-- go up a floor
         w_stop <= '0';  wait for k_clk_period;
             assert w_floor = x"3" report "bad up from floor2" severity failure;
+		
 		-- try waiting on a floor
         w_stop <= '1';  wait for k_clk_period * 2;
             assert w_floor = x"3" report "bad wait on floor3" severity failure;
+		
 		--  go up again
+		w_stop <= '0';  wait for k_clk_period;
+            assert w_floor = x"4" report "bad up from floor3" severity failure;
 		
 		-- go back down one floor
+		w_up_down <= '0'; 
+		w_stop <= '0';  wait for k_clk_period;
+            assert w_floor = x"3" report "bad down from floor4" severity failure;
 		
 		-- go up the rest of the way
+		w_up_down <= '1';
+		w_stop <= '0';  wait for k_clk_period;
+            assert w_floor = x"4" report "bad up from floor3" severity failure;
 		
 		-- stop at top
-        
+        w_stop <= '1';  wait for k_clk_period * 2;
+            assert w_floor = x"4" report "bad wait on top floor" severity failure;
+            
         -- go all the way down DOWN (how many clock cycles should that take?)
         w_up_down <= '0'; 
+        w_stop <= '1';  wait for k_clk_period * 3;
+            assert w_floor = x"1" report "bad wait way down" severity failure;
+            
   
 		  	
 		wait; -- wait forever
